be126e83c2a697260b330673ce6d3e614c87efa1
Karmvir <jadejakarmvir12@gmail.com>
2024-09-20 10:07:50 +0530

    Here added the cors middlewares and the webtoken secret key

---
+++ Backend/config.js
@@ -0,0 +1,2 @@
+const JWT_SECRET = "karmvirsecret";
+module.exports = JWT_SECRET;
--- Backend/index.js
+++ Backend/index.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const { mainRouter } = require("./routes/index.js");
 const User = require("./db");
+const cors = require("cors");
 const app = express();
 const port = 3000;
+app.use(cors());
+app.use(express.json());
+
+//As the backend and frontend are in different directory then Cors middleware is needed
 
 //Also used to route request with certain URL
 app.use("/api/v1", mainRouter);
