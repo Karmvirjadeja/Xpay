8ba78d2643d9f1f5d37ef74a34ec4e8226a31350
Karmvir <jadejakarmvir12@gmail.com>
2024-09-22 11:49:44 +0530

    Here set up the App.jsx added the react router and ceated the pages folder with dashbboard, sendMoney,Heading,Subheading,InputBox the component and created the reusable components like Buttons headings inptbox subheading and Warning box

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,10 +1,26 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+
+import { Signup } from "../src/pages/Signup";
+import { Signin } from "../src/pages/Signin";
+import { Dashboard } from "../src/pages/Dashboard";
+import { SendMoney } from "../src/pages/SendMoney";
+
 import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-  return <></>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/signup" element={<Signup />}></Route>
+        <Route path="/signin" element={<Signin />}></Route>
+        <Route path="/dashboard" element={<Dashboard />}></Route>
+        <Route path="/send" element={<SendMoney />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
