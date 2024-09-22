1f34717f315e68d6ff397ebabe91418cd8d5e188
Karmvir <jadejakarmvir12@gmail.com>
2024-09-22 14:51:52 +0530

    Here added the the onChnage functionality onto the Signup page

--- Frontend/src/pages/Dashboard.jsx
+++ Frontend/src/pages/Dashboard.jsx
@@ -1,3 +1,15 @@
-export function Dashboard() {
-  return <div>DashboardComponent UnderConstruction</div>;
-}
+import { Appbar } from "../components/Appbar";
+import { Balance } from "../components/Balance";
+import { Users } from "../components/Users";
+
+export const Dashboard = () => {
+  return (
+    <div>
+      <Appbar />
+      <div className="m-8">
+        <Balance value={"10,000"} />
+        <Users />
+      </div>
+    </div>
+  );
+};
