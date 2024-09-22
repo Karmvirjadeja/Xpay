c248f61475d74868f2a00763da4a80a8b84a3780
Karmvir <jadejakarmvir12@gmail.com>
2024-09-22 16:10:06 +0530

    Here added the filter parameter

--- Frontend/src/components/Users.jsx
+++ Frontend/src/components/Users.jsx
@@ -4,16 +4,22 @@ import axios from "axios";
 export const Users = () => {
   // Replace with backend call
   const [users, setUsers] = useState([]);
+  const [filter, setFilter] = useState("");
   useEffect(() => {
-    axios.get("http://localhost:3000/api/v1/users/bulk").then((response) => {
-      setUsers(response.data.users);
-    });
-  });
+    axios
+      .get("http://localhost:3000/api/v1/users/bulk?filter=" + filter)
+      .then((response) => {
+        setUsers(response.data.users);
+      });
+  }, [filter]);
   return (
     <>
       <div className="font-bold mt-6 text-lg">Users</div>
       <div className="my-2">
         <input
+          onChange={(e) => {
+            setFilter(e.target.value);
+          }}
           type="text"
           placeholder="Search users..."
           className="w-full px-2 py-1 border rounded border-slate-200"
