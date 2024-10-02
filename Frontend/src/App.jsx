e1647b42d430805a3a58fb545c6d287e43f82407
Karmvir <jadejakarmvir12@gmail.com>
2024-10-02 21:44:40 +0530

    Here added the  Make Transfer option

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -12,14 +12,43 @@ import "./App.css";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/signup" element={<Signup />}></Route>
-        <Route path="/signin" element={<Signin />}></Route>
-        <Route path="/dashboard" element={<Dashboard />}></Route>
-        <Route path="/send" element={<SendMoney />}></Route>
-      </Routes>
-    </BrowserRouter>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/signup" element={<Signup />}></Route>
+          <Route path="/signin" element={<Signin />}></Route>
+          <Route path="/dashboard" element={<Dashboard />}></Route>
+          <Route path="/send" element={<SendMoney />}></Route>
+        </Routes>
+        <Route path="/notification" element={<ButtonPressed />}></Route>
+      </BrowserRouter>
+      <ButtonPressed />
+    </>
+  );
+}
+
+function ButtonPressed() {
+  return (
+    <div
+      className="bg-teal-100 border-t-4 border-teal-500 rounded-b text-teal-900 px-4 py-3 shadow-md"
+      role="alert"
+    >
+      <div className="flex">
+        <div className="py-1">
+          <svg
+            className="fill-current h-6 w-6 text-teal-500 mr-4"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 20"
+          >
+            <path d="M2.93 17.07A10 10 0 1 1 17.07 2.93 10 10 0 0 1 2.93 17.07zm12.73-1.41A8 8 0 1 0 4.34 4.34a8 8 0 0 0 11.32 11.32zM9 11V9h2v6H9v-4zm0-6h2v2H9V5z" />
+          </svg>
+        </div>
+        <div>
+          <p className="font-bold">Success</p>
+          <p className="text-sm">Transfered Securely.</p>
+        </div>
+      </div>
+    </div>
   );
 }
 
--- Frontend/src/pages/SendMoney.jsx
+++ Frontend/src/pages/SendMoney.jsx
@@ -1,7 +1,10 @@
+import { useState } from "react";
 export const SendMoney = () => {
+  const [amount, setAmount] = useState(0);
+
   return (
     <div className="flex justify-center h-screen bg-gray-100">
-      <div className="h-full flex flex-col justify-center">
+      <div className="h-full flex sflex-col justify-center">
         <div className="border h-min text-card-foreground max-w-md p-4 space-y-8 w-96 bg-white shadow-lg rounded-lg">
           <div className="flex flex-col space-y-1.5 p-6">
             <h2 className="text-3xl font-bold text-center">Send Money</h2>
@@ -26,10 +29,18 @@ export const SendMoney = () => {
                   className="flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm"
                   id="amount"
                   placeholder="Enter amount"
+                  onChange={(e) => {
+                    setAmount(e.target.value);
+                  }}
                 />
               </div>
-              <button className="justify-center rounded-md text-sm font-medium ring-offset-background transition-colors h-10 px-4 py-2 w-full bg-green-500 text-white">
-                Initiate Transfer
+              <button
+                className="justify-center rounded-md text-sm font-medium ring-offset-background transition-colors h-10 px-4 py-2 w-full bg-green-500 text-white"
+                onClick={() => {
+                  console.log(amount);
+                }}
+              >
+                Make transfer
               </button>
             </div>
           </div>
